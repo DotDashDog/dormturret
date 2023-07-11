6ab727005275b9feaf694422491e4209798ced7b
Linus Upson <kit@upson.org>
2023-07-11 13:20:29 -0700

    Targeting box displays added

--- defensemode.py
+++ defensemode.py
@@ -54,16 +54,51 @@ def latest_whitelist_encodings(whitelist_dir, state_file):
 
 
 #%%
+#* Load whitelist encodings, updating (with a few runs of the model) if necessary
 whitelist_encs = latest_whitelist_encodings(whitelist_dir, state_file)
 
 #%%
+#* Load the image to evaluate
+fov = face_recognition.load_image_file(os.path.join("test_images", "bidenpelosi1.jpeg"))
 
-image1 = face_recognition.load_image_file(os.path.join("test_images", "biden1.jpg"))
-enc1 = face_recognition.api.face_encodings(image1)[0]
+locations = face_recognition.api.face_locations(
+    fov,
+    number_of_times_to_upsample=3, #* 1 is default, higher finds smaller faces. Tuning needed
+    model="hog", #* Options: "hog" (faster on cpu, less accurate), "cnn" (faster on gpu, more accurate)
+)
+
+#* Location is a tuple in the format (top, right, bottom, left) (bounding box)
+
+face_encodings = face_recognition.api.face_encodings(fov, locations)
+print(f"{len(face_encodings)} faces detected.")
 
 
 # %%
 
-face_recognition.api.face_distance(whitelist_encs, enc1)
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
+ax = plt.gca()
+
+ax.imshow(fov)
+
+def isWhitelisted(encoding, whitelist_encodings):
+    distances = face_recognition.api.face_distance(whitelist_encodings, encoding)
+    return np.any(distances < 0.6)
+
+def faceCenter(box):
+    return (box[1] + box[3])/2, (box[0] + box[2])/2 
+
+for i, face in enumerate(face_encodings):
+    box = locations[i]
+    if not isWhitelisted(face, whitelist_encs):
+        #* Add box around face
+        rect = patches.Rectangle((box[3], box[2]), box[1]-box[3], box[0]-box[2], linewidth=1, edgecolor='r', facecolor='none')
+        print(f"Target found. Face center at {faceCenter(box)}")
+    else:
+        rect = patches.Rectangle((box[3], box[2]), box[1]-box[3], box[0]-box[2], linewidth=1, edgecolor='g', facecolor='none')
+        print(f"Non-Target found. Face center at {faceCenter(box)}")
+    ax.add_patch(rect)
+
+plt.show()
 # %%
